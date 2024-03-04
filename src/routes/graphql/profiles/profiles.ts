681869701097cb1e58b20c9dec9833fbf26fdc67
ImmelstronDev <wishandra.afk@gmail.com>
2024-03-04 12:15:46 +0300

    feat: implement addition fields  usertype and profiletype

--- src/routes/graphql/profiles/profiles.ts
+++ src/routes/graphql/profiles/profiles.ts
@@ -1,6 +1,10 @@
 import { GraphQLBoolean, GraphQLInt, GraphQLList, GraphQLObjectType } from 'graphql';
 import { UUIDType } from '../types/uuid.js';
 import { MemberTypeId } from '../types/memberTypeId.js';
+import { PrismaClient } from '@prisma/client';
+import { memberType } from '../member-types/member-type.js';
+
+const prisma = new PrismaClient();
 
 export const profileType = new GraphQLObjectType({
   name: 'Profile',
@@ -10,6 +14,15 @@ export const profileType = new GraphQLObjectType({
     yearOfBirth: { type: GraphQLInt },
     userId: { type: UUIDType },
     memberTypeId: { type: MemberTypeId },
+    memberType: {
+      type: memberType,
+      resolve: async (parent: { memberTypeId: string }) => {
+        const memberType = await prisma.memberType.findUnique({
+          where: { id: parent.memberTypeId },
+        });
+        return memberType;
+      },
+    },
   }),
 });
 
--- src/routes/graphql/users/users.ts
+++ src/routes/graphql/users/users.ts
@@ -1,5 +1,10 @@
 import { GraphQLFloat, GraphQLList, GraphQLObjectType, GraphQLString } from 'graphql';
 import { UUIDType } from '../types/uuid.js';
+import { PrismaClient } from '@prisma/client';
+import { profileType } from '../profiles/profiles.js';
+import { postsType } from '../posts/posts.js';
+
+const prisma = new PrismaClient();
 
 export const userType = new GraphQLObjectType({
   name: 'User',
@@ -7,6 +12,20 @@ export const userType = new GraphQLObjectType({
     id: { type: UUIDType },
     name: { type: GraphQLString },
     balance: { type: GraphQLFloat },
+    profile: {
+      type: profileType,
+      resolve: async (parent: { id: string }) => {
+        const profile = await prisma.profile.findUnique({ where: { userId: parent.id } });
+        return profile;
+      },
+    },
+    posts: {
+      type: postsType,
+      resolve: async (parent: { id: string }) => {
+        const posts = await prisma.post.findMany({ where: { authorId: parent.id } });
+        return posts;
+      },
+    },
   }),
 });
 
